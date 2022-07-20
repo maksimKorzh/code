9bdf6934859f429fcae72c907c8ed7e1761c5c9b
Code Monkey King <freesoft.for.people@gmail.com>
2022-07-21 01:18:55 +0300

    debugginh arrow up/down keys

--- code.py
+++ code.py
@@ -10,6 +10,7 @@ def ctrl(c): return ((c) & 0x1f)
 
 COLS = 80
 ROWS = 24
+usrx = 0
 curx = 0
 cury = 0
 curs = 0
@@ -42,8 +43,35 @@ while(True):
   c = screen.getch()
   if (c == -1): continue
   if c == ctrl(ord('q')): break
-  elif c == curses.KEY_LEFT: move_left()
-  elif c == curses.KEY_RIGHT: move_right()
+  elif c == curses.KEY_LEFT:
+    move_left()
+    usrx = curx
+  elif c == curses.KEY_RIGHT:
+    move_right()
+    usrx = curx
+  elif c == curses.KEY_UP:
+    if cury:
+      oldx = curx
+      oldy = cury
+      move_left()
+      while(oldx != curx):
+        move_left()
+        if cury < oldy and curx < oldx: break
+        if curx == usrx and cury != oldy: break
+  elif c == curses.KEY_DOWN:
+    if (1):
+      oldx = curx
+      oldy = cury
+      move_right()
+      while(oldx != curx):
+        move_right()
+        if cury > oldy and buff[curs] == ord('\n'): break
+      if curx != usrx:
+        pass
+        #if buff[curs] != ord('\n'):
+        #  while (curx != usrx): move_right();
+
+        
   elif c == curses.KEY_BACKSPACE:
     move_left()
     if len(buff): del buff[curs]
@@ -52,6 +80,7 @@ while(True):
     if c == ord('\n'): cury += 1; curx = -1;
     buff.insert(curs, c); curs += 1
     curx += 1
+    usrx = curx
 
   screen.move(0, 0)
   curses.curs_set(0)
