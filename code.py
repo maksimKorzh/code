039d2d2dc5cd6b0ef5a5f094671b92ec7e6dcf4b
Code Monkey King <freesoft.for.people@gmail.com>
2022-07-23 11:30:51 +0300

    added scroll down, debugging scroll up

--- code.py
+++ code.py
@@ -6,15 +6,18 @@ class Editor():
     self.screen = curses.initscr()
     self.screen.keypad(True)
     self.screen.nodelay(1)
-    curses.raw()
-    curses.noecho()
     self.COLS = 80
     self.ROWS = 24
+    self.offx = 0
+    self.offy = 0
     self.usrx = 0
     self.curx = 0
     self.cury = 0
     self.curs = 0
     self.buff = []
+    self.total_lines = 0
+    curses.raw()
+    curses.noecho()
 
   def ctrl(self, c): return ((c) & 0x1f)
   def move_right(self):    
@@ -24,6 +27,9 @@ class Editor():
         self.curx = 0
       else: self.curx += 1
       self.curs += 1;
+    if self.cury == self.ROWS:
+      self.cury -= 1
+      self.scroll_down()
 
   def move_left(self):
     if self.curs:
@@ -31,18 +37,18 @@ class Editor():
       if self.curx: self.curx -= 1
       else:
         if self.cury: self.cury -= 1
-        if self.cury == 0: self.curx = self.curs
+        if self.cury == 0:             
+          self.curx = self.curs
         else:
           count = self.curs - 1
           while(self.buff[count] != ord('\n')): count -= 1
           self.curx = self.curs - count - 1
-
+    
   def move_up(self):
     self.move_home()
     self.move_left()
-    while self.curx > self.usrx:
-      self.move_left()
-
+    while self.curx > self.usrx: self.move_left()
+        
   def move_down(self):
     self.move_end()
     self.move_right()
@@ -74,15 +80,46 @@ class Editor():
 
   def insert_char(self, c):
     if c == ord('\n'):
-      self.cury += 1
-      self.curx = -1;
+      self.total_lines += 1
+      if self.cury < self.ROWS - 1: self.cury += 1
+      self.curx = -1
+      self.screen.clrtoeol()
     self.buff.insert(self.curs, c); self.curs += 1
     self.curx += 1
     self.usrx = self.curx
 
-  def exit(self):
-    curses.endwin()
-    sys.exit(0)
+  def print_buffer(self):
+    '''
+    start printing chars only when current
+    buffer row is >= than the row offset
+    stop printing rows when printed 24 rows
+    '''
+    
+    
+    
+    rows = 0
+    for c in self.buff:
+      if rows >= self.offy and rows < self.ROWS + self.offy:
+        try: self.screen.addch(c)
+        except: pass
+
+      if c == ord('\n'):
+        rows += 1
+        if rows > self.ROWS + self.offy-1: break
+  
+  def scroll_up(self): self.offy -= 1
+  def scroll_down(self): self.offy += 1
+
+  def update_screen(self):
+    self.screen.move(0, 0)
+    curses.curs_set(0)
+    self.print_buffer()
+    
+    #if self.cury < self.ROWS-1 and self.curx < self.COLS-1:
+    self.screen.move(self.cury, self.curx)
+
+    curses.curs_set(1)
+    self.screen.refresh()
 
   def read_keyboard(self):
     self.usrx = self.usrx
@@ -96,16 +133,21 @@ class Editor():
     elif c == curses.KEY_UP: self.move_up()
     elif c == curses.KEY_DOWN: self.move_down()
     elif c == curses.KEY_BACKSPACE: self.delete_char()
+    elif c == curses.KEY_PPAGE: self.scroll_up()
+    elif c == curses.KEY_NPAGE: self.scroll_down()
     else: self.insert_char(c)
 
-  def update_screen(self):
-    self.screen.move(0, 0)
-    curses.curs_set(0)
-    [self.screen.addch(c) for c in self.buff]
-    self.screen.move(self.cury, self.curx)
-    curses.curs_set(1)
-    self.screen.refresh()
-  
+  def open_file(self, filename):
+    with open('code.py') as f:
+      content = f.read()
+      self.buff = [ord(c) for c in content]
+      self.total_lines = len(content.split('\n')) - 1
+    self.update_screen()
+
+  def exit(self):
+    curses.endwin()
+    sys.exit(0)
+
   def start(self):
     while(True):
       self.read_keyboard()
@@ -113,5 +155,7 @@ class Editor():
 
 if __name__ == '__main__':
   editor = Editor()
+  editor.open_file('code.py')
   editor.start()
+
 
